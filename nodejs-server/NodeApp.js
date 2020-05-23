3a4d3e91fd96f592d048022a348be105cee1cbcb
Srivatsan <srivatsan.vn@gmail.com>
2020-05-23 12:59:49 -0400

    Modifying the case for train router

--- nodejs-server/NodeApp.js
+++ nodejs-server/NodeApp.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //import routers
-const trainrouter = require('./routes/trainrouter');
+const trainrouter = require('./routes/Trainrouter');
 app.use("/", trainrouter);
  
 mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true },() =>
