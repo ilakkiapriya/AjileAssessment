bb971468122ba572fd70381bda3abf8067b76497
Srivatsan <srivatsan.vn@gmail.com>
2020-08-01 17:33:36 -0400

    Login page as functional component

--- react-module/src/component/LoginContainer/LoginPage.js
+++ react-module/src/component/LoginContainer/LoginPage.js
@@ -1,167 +1,123 @@
 import React, { Component } from "react";
 import "./LoginPage.css";
 
-const initialState = {
-    firstName: null,
-    lastName: null,
-    email: null,
-    password: null,
+
+export default function LoginPage() {
+
+  const [loginState, setLoginState] = React.useState({
+    firstName: null, lastName: null, email: null, password: null,
     formErrors: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: ""
+      firstName: "", lastName: "",   email: "",   password: ""
     }
-
-};
-const emailRegex = RegExp(
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-);
-
-const formValid = ({ formErrors, ...rest }) => {
-  let valid = true;
-
-  // validate form errors being empty
-  Object.values(formErrors).forEach(val => {
-    val.length > 0 && (valid = false);
   });
 
-  // validate the form was filled out
-  Object.values(rest).forEach(val => {
-    val === null && (valid = false);
-  });
-  console.log("valid", valid);
-  return valid;
-};
+  const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+  
+  const handleChange = e => {
+    console.log("Handle change - loginState ", loginState);
 
-class LoginPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = initialState;
-  }
-
-  handleSubmit = e => {
-
-    if (formValid(this.state)) {
-      console.log(`
-        --SUBMITTING--
-        First Name: ${this.state.firstName}
-        Last Name: ${this.state.lastName}
-        Email: ${this.state.email}
-        Password: ${this.state.password}
-      `);
-      this.setState(initialState);
-    } else {
-      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
-    }
-  };
-
-  handleChange = e => {
     e.preventDefault();
     const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
+    //let newLoginState = { ...loginState };
+    let newLoginState = loginState ;
 
     switch (name) {
       case "firstName":
-        formErrors.firstName =
+        newLoginState.formErrors.firstName =
           value.length < 3 ? "minimum 3 characaters required" : "";
         break;
       case "lastName":
-        formErrors.lastName =
+        newLoginState.formErrors.lastName =
           value.length < 3 ? "minimum 3 characaters required" : "";
         break;
       case "email":
-        formErrors.email = emailRegex.test(value)
+        newLoginState.formErrors.email = emailRegex.test(value)
           ? ""
           : "invalid email address";
         break;
       case "password":
-        formErrors.password =
+        newLoginState.formErrors.password =
           value.length < 6 ? "minimum 6 characaters required" : "";
         break;
       default:
         break;
     }
-
-    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+    newLoginState[name] = value;
+    setLoginState(newLoginState);
   };
 
-  render() {
-    const { formErrors } = this.state;
+  const handleSubmit = (e) => {
+    console.log("Event is  " , e);
 
-    return (
-      <div className="wrapper">
-        <div className="form-wrapper">
-          <h1>Login</h1>
-          <form onSubmit={this.handleSubmit} noValidate>
-            <div className="firstName">
-              <label htmlFor="firstName">First Name</label>
-              <input
-                className={formErrors.firstName.length > 0 ? "error" : null}
-                placeholder="First Name"
-                type="text"
-                name="firstName"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.firstName.length > 0 && (
-                <span className="errorMessage">{formErrors.firstName}</span>
-              )}
-            </div>
-            <div className="lastName">
-              <label htmlFor="lastName">Last Name</label>
-              <input
-                className={formErrors.lastName.length > 0 ? "error" : null}
-                placeholder="Last Name"
-                type="text"
-                name="lastName"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.lastName.length > 0 && (
-                <span className="errorMessage">{formErrors.lastName}</span>
-              )}
-            </div>
-            <div className="email">
-              <label htmlFor="email">Email</label>
-              <input
-                className={formErrors.email.length > 0 ? "error" : null}
-                placeholder="Email"
-                type="email"
-                name="email"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.email.length > 0 && (
-                <span className="errorMessage">{formErrors.email}</span>
-              )}
-            </div>
-            <div className="password">
-              <label htmlFor="password">Password</label>
-              <input
-                className={formErrors.password.length > 0 ? "error" : null}
-                placeholder="Password"
-                type="password"
-                name="password"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.password.length > 0 && (
-                <span className="errorMessage">{formErrors.password}</span>
-              )}
-            </div>
-            <div className="login">
+    if (formValid()) {
+      console.log("login state is valid " , loginState);
+      //this.setState(initialState);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
+  };
+
+  const formValid = () => {
+    let valid = true;
+    console.log("Validate login state  " , loginState);
+
+    const { formErrors, ...rest } = loginState;
+    console.log("formErrors  " , formErrors);
+    console.log("rest  " , rest);
+
+    Object.values(formErrors).forEach(val => {
+      val.length > 0 && (valid = false);
+    });
+  
+    // validate the form was filled out
+    Object.values(rest).forEach(val => {
+      val === null && (valid = false);
+    });
+  
+    return valid;
+  };
+  
+
+
+ const fetchFormField = (cname, clabel, ctype) => {
+    return   (  <div className={cname}>
+    <label htmlFor={cname}>{clabel}</label>
+    <input
+      className={loginState.formErrors[cname].length > 0 ? "error" : null}
+      placeholder={clabel}
+      type={ctype}
+      name={cname}
+      noValidate
+      onChange={handleChange}
+    />
+    {loginState.formErrors[cname].length > 0 && (
+      <span className="errorMessage">{loginState.formErrors[cname]}</span>
+    )}
+  </div> )
+
+  };
+
+  return (
+    <div className="wrapper">
+    <div className="form-wrapper">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} noValidate>
+        {fetchFormField("firstName", "First Name", "text")}
+        {fetchFormField("lastName", "Last Name", "text")}
+        {fetchFormField("email", "Email", "email")}
+        {fetchFormField("password", "Password", "password")}
+        <div className="login">
               <button className="loginbutton" type="submit">Login</button>
-              </div>
-              <div className="register" >
-              <button className="regbutton" type="register">Register</button>
-            </div>
-          </form>
         </div>
-      </div>
-    );
-  }
-}
+        <div className="register" >
+              <button className="regbutton" type="register">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
 
-export default LoginPage;
+  );
+
+};
